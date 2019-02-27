f5605eff1ff7f55cdfbd39bd22a44ebe1f700b63
FancyChipotle <zbryantaylor@gmail.com>
2019-02-26 18:28:00 -0700

    Get User Auth Service Working
    Can login, create account, reset password.

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG = {
+    apiKey: 'AIzaSyBMXYd9FcgSGRStoCrtdWjchz673xIpheA',
+    authDomain: 'eventmanager-bb1f9.firebaseapp.com',
+    databaseURL: 'https://eventmanager-bb1f9.firebaseio.com',
+    projectId: 'eventmanager-bb1f9',
+    storageBucket: 'eventmanager-bb1f9.appspot.com',
+    messagingSenderId: '635299336735'
+  };
