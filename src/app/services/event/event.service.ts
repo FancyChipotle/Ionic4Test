10d5b443c2b8cbe2fbde19bb22d3cac138e406ab
FancyChipotle <zbryantaylor@gmail.com>
2019-04-09 18:22:45 -0600

    Finish Ionic 4 Test
    Success. Except firestore transaction seems to not work. Also, XCode still sucks.

--- src/app/services/event/event.service.ts
+++ src/app/services/event/event.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/storage';
 
 @Injectable({
   providedIn: 'root'
@@ -35,5 +36,39 @@ export class EventService {
 
   getEventDetail(eventID: string): firebase.firestore.DocumentReference {
     return this.eventListRef.doc(eventID);
+  }
+
+  addGuest(guestName: string, eventID: string, eventPrice: number,
+    guestPicture: string = null): Promise<void> {
+      return this.eventListRef
+        .doc(eventID)
+        .collection('guestList')
+        .add({ guestName })
+        .then((newGuest) => {
+          return firebase.firestore().runTransaction(transaction => {
+            return transaction.get(this.eventListRef.doc(eventID)).then(eventDoc => {
+              const newRevenue = eventDoc.data().revenue + eventPrice;
+              transaction.update(this.eventListRef.doc(eventID), { revenue: newRevenue });
+
+              if (guestPicture != null) {
+                const storageRef = firebase
+                  .storage()
+                  .ref(`/guestProfile/${newGuest.id}/profilePicture.png`);
+
+                return storageRef
+                  .putString(guestPicture, 'base64', { contentType: 'image/png' })
+                  .then(() => {
+                    return storageRef.getDownloadURL().then(downloadURL => {
+                      return this.eventListRef
+                        .doc(eventID)
+                        .collection('guestList')
+                        .doc(newGuest.id)
+                        .update({ profilePicture: downloadURL });
+                    });
+                  });
+              }
+            });
+          });
+        });
   }
 }
